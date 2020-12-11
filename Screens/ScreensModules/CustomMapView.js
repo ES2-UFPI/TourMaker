0c1203915090941aa63e1fdc2592d1862a444105
metangross <mettangroos@gmail.com>
2020-12-11 17:00:15 -0300

    Implementado issues #2 e atualizado #10

--- Screens/ScreensModules/CustomMapView.js
+++ Screens/ScreensModules/CustomMapView.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-
+import { StyleSheet, View, Text, PermissionsAndroid} from 'react-native';
+import MapView from 'react-native-maps'
+import Permissions from '../../APIs/Permissions'
 class CustomMapView extends Component{
+    state ={
+        region: null,
+        userLocationPermission: false
+    }
+
+    onRegionChange = (region) => {
+        this.setState ({ region });
+    }
+    onUserLocationChange = (location) => {
+        let UserCoords = location.nativeEvent.coordinate
+        if(!this.state.userFlag){
+            this.setState({
+                userFlag: true,
+                region:{
+                    latitude: UserCoords.latitude,
+                    latitudeDelta: 0.05,
+                    longitude: UserCoords.longitude,
+                    longitudeDelta: 0.05,
+                }
+            })
+        }
+    }
+    callbackPermissionLocation = (status) => {
+        console.log(status)
+        if(status != "granted"){
+            alert("Permissão de localização não foi concedida!")
+        }
+        else{
+            this.setState(
+                {userLocationPermission: true}
+            )
+        }
+    }
+    componentDidMount(){
+
+        Permissions.verifyLocationPermission(this.callbackPermissionLocation)
+    }
     render(){
         return(
             <View style={styles.CustomMapView}>
-                <Text>Mapa a ser implementado</Text>
+                <MapView
+                style={styles.map}
+                onRegionChangeComplete={this.onRegionChange}
+                region={this.state.region}
+                onUserLocationChange={this.onUserLocationChange}
+                showsUserLocation={this.state.userLocationPermission}
+                followsUserLocation={this.state.userLocationPermission}
+                ></MapView>
             </View>
         )
     }
 }
-
 const styles = StyleSheet.create({
     CustomMapView: {
       backgroundColor: 'yellow',
@@ -20,5 +63,9 @@ const styles = StyleSheet.create({
       width: '100%',
       height: 400
     },
+    map: {
+        width:'100%',
+        height:'100%',
+    }
   });
 export default CustomMapView
