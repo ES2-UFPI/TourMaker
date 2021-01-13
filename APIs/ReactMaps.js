6846be100a2c2d3bf77afac480bd8537c08a0735
Mayron Junior <mayronjunior5@ufpi.edu.br>
2021-01-13 11:41:51 -0300

    Modificação da API do maps para usar a API_KEY que esta no Firebase

--- APIs/ReactMaps.js
+++ APIs/ReactMaps.js
@@ -1,17 +1,58 @@
-YOUR_API_KEY = 'AIzaSyAnU7Zr8HxyX9pGfcc7hytRKdfQG9AXk5Q'
+import FirebaseFunctions from './Firebase';
 
 export default class ReactMaps {
-    
-    static getLocationByName = (nome, callback) => {
-        var encodedName = nome.replace(/ /gi, '+')
-        const url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?input=' + encodedName + '&radius=10' + '&key=' + YOUR_API_KEY
-        fetch(url)
+
+  static getLocationByName = (nome, callback) => {
+    FirebaseFunctions.recoverKey(key => {
+      this.getLocationByNameIntern(nome, key, callback)
+    })
+  }
+  static getLocationByNameIntern = (nome, API_KEY, callback) => {
+    var encodedName = nome.replace(/ /gi, '+')
+    const url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?input=' + encodedName + '&radius=10' + '&key=' + API_KEY
+    fetch(url).then(res => {
+      return res.json()
+    }).then(res => {
+      var places = [] // This Array WIll contain locations received from google
+      for (let googlePlace of res.results) {
+        var place = {}
+        var lat = googlePlace.geometry.location.lat;
+        var lng = googlePlace.geometry.location.lng;
+        var coordinate = {
+          latitude: lat,
+          longitude: lng,
+        }
+
+        var gallery = []
+
+        place['placeTypes'] = googlePlace.types
+        place['coordinate'] = coordinate
+        place['placeId'] = googlePlace.place_id
+        place['placeName'] = googlePlace.name
+        place['rating'] = googlePlace.rating
+
+        places.push(place);
+      }
+      callback(places)
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  static getNearbyLocation = (latitude, longitude, radius, callback) => {
+    FirebaseFunctions.recoverKey(key => {
+      this.getNearbyLocationIntern(latitude, longitude, radius, key, callback)
+    })
+  }
+  static getNearbyLocationIntern = (latitude, longitude, radius, API_KEY, callback) => {
+    const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=' + radius + '&key=' + API_KEY
+    fetch(url)
       .then(res => {
         return res.json()
       })
       .then(res => {
         var places = [] // This Array WIll contain locations received from google
-        for(let googlePlace of res.results) {
+        for (let googlePlace of res.results) {
           var place = {}
           var lat = googlePlace.geometry.location.lat;
           var lng = googlePlace.geometry.location.lng;
@@ -19,7 +60,7 @@ export default class ReactMaps {
             latitude: lat,
             longitude: lng,
           }
-          
+
           var gallery = []
 
           place['placeTypes'] = googlePlace.types
@@ -35,76 +76,45 @@ export default class ReactMaps {
       .catch(error => {
         console.log(error);
       });
-    
-  }
- 
-    static getNearbyLocation = (latitude,longitude,radius,callback) => {  
-      const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=' + radius + '&key=' + YOUR_API_KEY
-      fetch(url)
-    .then(res => {
-      return res.json()
-    })
-    .then(res => {
-      var places = [] // This Array WIll contain locations received from google
-      for(let googlePlace of res.results) {
-        var place = {}
-        var lat = googlePlace.geometry.location.lat;
-        var lng = googlePlace.geometry.location.lng;
-        var coordinate = {
-          latitude: lat,
-          longitude: lng,
-        }
-        
-        var gallery = []
-
-        place['placeTypes'] = googlePlace.types
-        place['coordinate'] = coordinate
-        place['placeId'] = googlePlace.place_id
-        place['placeName'] = googlePlace.name
-        place['rating'] = googlePlace.rating
-
-        places.push(place);
-      }
-      callback(places)
-    })
-    .catch(error => {
-      console.log(error);
-    });
 
   }
 
-    static getLocationByType = (type, callback) => {
-      const url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?type=' + type + '&radius=10' + '&key=' + YOUR_API_KEY
-      fetch(url)
-    .then(res => {
-      return res.json()
+  static getLocationByType = (type, callback) => {
+    FirebaseFunctions.recoverKey(key => {
+      this.getLocationByTypeIntern(type, key, callback)
     })
-    .then(res => {
-      var places = [] // This Array WIll contain locations received from google
-      for(let googlePlace of res.results) {
-        var place = {}
-        var lat = googlePlace.geometry.location.lat;
-        var lng = googlePlace.geometry.location.lng;
-        var coordinate = {
-          latitude: lat,
-          longitude: lng,
+  }
+  static getLocationByTypeIntern = (type, API_KEY, callback) => {
+    const url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?type=' + type + '&radius=10' + '&key=' + API_KEY
+    fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        var places = [] // This Array WIll contain locations received from google
+        for (let googlePlace of res.results) {
+          var place = {}
+          var lat = googlePlace.geometry.location.lat;
+          var lng = googlePlace.geometry.location.lng;
+          var coordinate = {
+            latitude: lat,
+            longitude: lng,
+          }
+
+          var gallery = []
+
+          place['placeTypes'] = googlePlace.types
+          place['coordinate'] = coordinate
+          place['placeId'] = googlePlace.place_id
+          place['placeName'] = googlePlace.name
+          place['rating'] = googlePlace.rating
+
+          places.push(place);
         }
-        
-        var gallery = []
-
-        place['placeTypes'] = googlePlace.types
-        place['coordinate'] = coordinate
-        place['placeId'] = googlePlace.place_id
-        place['placeName'] = googlePlace.name
-        place['rating'] = googlePlace.rating
-
-        places.push(place);
-      }
-      callback(places)
-    })
-    .catch(error => {
-      console.log(error);
-    });
-
+        callback(places)
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
