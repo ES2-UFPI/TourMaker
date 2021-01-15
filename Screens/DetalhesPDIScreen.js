7602ffe715ecc8566fc564e5e61c58aca728ba7b
metangross <mettangroos@gmail.com>
2021-01-15 12:47:57 -0300

    Implementação parcial issue #28

--- Screens/DetalhesPDIScreen.js
+++ Screens/DetalhesPDIScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 
@@ -14,25 +14,88 @@ class DetalhesPDIScreen extends Component{
         super(props)
         this.state ={
             nome : props.route.params.PDI.placeName,
-            address : props.route.params.PDI.formatted_address,
+            address : props.route.params.PDI.formatted_address.split(","),
             isOpen : props.route.params.PDI.opening_hours,
+            gallery : null,
             //isOpen : props.route.params.PDI.opening_hours.open_now === true ? "Aberto" : "Fechado",
-            comments : []
+            comments : [],
+            tableHead: ["Comentários", "Avaliação"],
         }
-        //try {
-        //    this.setState({
-        //        comments : FirebaseFunctions.returnComments(props.route.params.PDI.placeID)
-        //    })
-            //console.log(this.state.comments)
-        //} catch (error) {
-        //    console.log(error)
-        //}
-        //console.log(this.state.nome)
+        /*
+        try {
+            FirebaseFunctions.returnComments(props.route.params.PDI.placeID, (result) => {
+                this.setState({
+                    comments : result
+                })
+            })
+            console.log(this.state.comments)
+        } catch (error) {
+            console.log(error)
+        }
+        */
+        ReactMaps.getPhotoByReference(props.route.params.PDI.gallery, (result) => {
+            this.setState({
+                gallery : {
+                    uri: result
+                }
+            })
+        })
     }
+
+
+
     render(){
+        var conteudo = "Este local "
+        if (this.state.isOpen != undefined){
+            conteudo += "está " + (this.state.isOpen.open_now ? "Aberto" : "Fechado")
+        }
+        else{
+            conteudo += "não fornece dados de horário de funcionamento"
+        }
+                    
         return(
-        <View>
-            <Text>Isso é um Teste</Text>
+        <View style={styles.DetalhesPDIScreen}>
+            <Image source={this.state.gallery} style={styles.img}/>
+            <View style={styles.viewText}>            
+                <Text>{this.state.nome}</Text>
+                <View style={styles.viewAddress}>
+                {
+                    
+                    this.state.address.map((data, index) => (
+                        <Text key={index}>{data}</Text>
+
+                    ))
+                }
+                </View>
+                <View style={styles.viewAddress}>
+                {
+                    conteudo.split(' ').map((data, index) => (
+                        <Text key={index}>{data} </Text>
+
+                    ))
+                }
+                </View>
+            </View>
+            
+            <Table borderStyle={{ borderColor: 'transparent' }}>
+                <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text} />
+                    {
+                        this.state.comments.map((rowData, index) =>(
+                            <TableWrapper key={index} style={styles.row}> 
+                                {
+                                    rowData.map((cellData, cellIndex) => (
+                                            <Cell key={cellIndex} data={cellData} textStyle={styles.text}></Cell>
+                                    ))
+
+                                }
+                            
+                            </TableWrapper>
+
+
+                        ))
+                    }
+            </Table>
+            
         </View>
         )
         
@@ -48,14 +111,13 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'flex-start',
     },
-    Buttons:{
-        color: '#1abc9c'
-    },
-    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
     head: { height: 40,width: 400, backgroundColor: '#1abc9c' },
     text: { margin: 6 },
     row: { flexDirection: 'row', backgroundColor: '#f1f8ff' },
     btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
-    btnText: { textAlign: 'center', color: '#fff' } 
+    btnText: { textAlign: 'center', color: '#fff' } ,
+    img: {width:200, height:200},
+    viewText:{padding: 10, alignItems:'center', backgroundColor:'#c5fcf1',alignSelf:'stretch', margin:5, borderRadius:50, borderWidth:1, borderColor:'#1abc9c'},
+    viewAddress:{alignSelf:'stretch', flexDirection:'row', flexWrap:'wrap', marginHorizontal:20, justifyContent:'center'}
   });
 export default DetalhesPDIScreen;
