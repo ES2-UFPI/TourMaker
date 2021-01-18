5583645f7492be5b2d6c83e197dcc8279f23f79b
metangross <mettangroos@gmail.com>
2021-01-18 19:21:24 -0300

    Implementado Issue #28

--- Screens/DetalhesPDIScreen.js
+++ Screens/DetalhesPDIScreen.js
@@ -18,24 +18,15 @@ class DetalhesPDIScreen extends Component{
             gallery : null,
             //isOpen : props.route.params.PDI.opening_hours.open_now === true ? "Aberto" : "Fechado",
             comments : [],
-            tableHead: ["Comentários", "Avaliação"],
+            tableHead: ["Nota", "Usuário", "Comentário"],
             id : props.route.params.PDI.placeId,
             avaliando : false,
             newRating : null,
             newComment : "",
+            mediaAval : 0
         }
-        /*
-        try {
-            FirebaseFunctions.returnComments(props.route.params.PDI.placeID, (result) => {
-                this.setState({
-                    comments : result
-                })
-            })
-            console.log(this.state.comments)
-        } catch (error) {
-            console.log(error)
-        }
-        */
+        this.recuperaComments()
+        
         ReactMaps.getPhotoByReference(props.route.params.PDI.gallery, (result) => {
             this.setState({
                 gallery : {
@@ -44,7 +35,26 @@ class DetalhesPDIScreen extends Component{
             })
         })
     }
-
+    recuperaComments(){
+        var comments = []
+        var qtd = 0
+        var mediaAval = 0
+        FirebaseFunctions.returnComments(this.state.id, (result) => {
+            var tam = result.length
+            for(var i = 0; i < tam; i++){
+                if(result[i].Corpo !== ''){
+                    comments.push([result[i].Avaliação,result[i].CPFUsuario, result[i].Corpo])
+                }
+                qtd += 1
+                mediaAval += result[i].Avaliação
+            }
+            mediaAval = mediaAval/qtd
+            this.setState({
+                comments,
+                mediaAval
+            })
+        })
+    }
     showAval(){
         this.setState(
             {
@@ -52,11 +62,23 @@ class DetalhesPDIScreen extends Component{
             }
         )
     }
+    setRating(value){
+        this.setState({
+            newRating : value+1
+        })
+    }
     sendAval(){
         var id = this.state.id
-        var newRating = this.state.newRating
+        var newRating = this.state.newRating !== null? this.state.newRating : 1
         var newComment = this.state.newComment
         FirebaseFunctions.writeComment(id, newRating, newComment)
+        alert('Avaliação enviada com sucesso!')
+        this.setState(
+            {
+                avaliando: !this.state.avaliando
+            }
+        )
+        this.recuperaComments()
     }
     render(){
         var conteudo = "Este local "
@@ -101,12 +123,7 @@ class DetalhesPDIScreen extends Component{
                         </TouchableOpacity>
                     ): (
                         <View style={{margin:10}}>
-                            <StarRating getRating = {(result) =>
-                                this.setState({
-                                    newRating : result+1
-                                })
-                            
-                            }/>
+                            <StarRating getRating = {(result) => this.setRating(result)}/>
                             <TextInput style={styles.txtInpt} editable numberOfLines={1} multiline={true} placeholder='Comentário(opcional)' onChangeText={(value) => 
                                 this.setState({
                                     newComment : value
@@ -134,7 +151,7 @@ class DetalhesPDIScreen extends Component{
                     {
                         this.state.comments.map((rowData, index) =>(
                             <TableWrapper key={index} style={styles.row}> 
-                                {
+                                {   
                                     rowData.map((cellData, cellIndex) => (
                                             <Cell key={cellIndex} data={cellData} textStyle={styles.text}></Cell>
                                     ))
@@ -163,12 +180,12 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'flex-start',
     },
-    head: { height: 40,width: 400, backgroundColor: '#1abc9c' },
+    head: { height: 40,width: 400, backgroundColor: '#1abc9c' , flex:1},
     text: { margin: 6 },
     row: { flexDirection: 'row', backgroundColor: '#f1f8ff' },
     btn: { backgroundColor: '#78B7BB',  borderRadius: 2 , borderRadius:10, marginTop:10, marginBottom:20, paddingHorizontal:5},
     btnText: { textAlign: 'center', color: '#fff' } ,
-    img: {width:200, height:200},
+    img: {width:200, height:200, marginTop:5},
     viewText:{padding: 10, alignItems:'center', backgroundColor:'#c5fcf1',alignSelf:'stretch', margin:5, borderRadius:50, borderWidth:1, borderColor:'#1abc9c'},
     viewAddress:{alignSelf:'stretch', flexDirection:'row', flexWrap:'wrap', marginHorizontal:20, justifyContent:'center'},
     txtInpt :{borderWidth:1, width:250, maxHeight:75, marginTop:10, paddingLeft:4}
