4d9ab5ee9e931cfbfc98b52b4bdf769c81a55257
metangross <mettangroos@gmail.com>
2021-01-25 11:50:50 -0300

    Implementado #56

--- APIs/AsyncStorage.js
+++ APIs/AsyncStorage.js
@@ -1,6 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default class AsyncStorage {
+export default class AsyncStorageFunctions {
     static exempleFunction = () =>{
         console.log('Essa aqui é um exemplo de função da API')
+    }
+
+    static storeData = async(Value, Chave) => {
+        try{
+            const jsonValue = JSON.stringify(Value)
+            await AsyncStorage.setItem(Chave,jsonValue)
+        }catch(e){
+
+        }
+    }
+    static retrieveData = async(Chave, callback) => {
+        try{
+            await AsyncStorage.getItem(Chave).then((value) => {
+                callback(value)
+            })
+        }catch(e){
+
+        }
     }
 };
